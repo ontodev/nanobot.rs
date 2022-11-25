23cea85685d9f833feca8133b8297918d92ed4a9
James A. Overton <james@overton.ca>
2022-11-25 20:30:53 +0000

    Run rustfmt on main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use clap::{command, Command};
-use std::path::Path;
 use std::fs;
+use std::path::Path;
 
-
-fn init() -> Result<&'static str,&'static str> {
-
+fn init() -> Result<&'static str, &'static str> {
     if Path::new("nanobot/nanobot.toml").exists() {
         Err("nanobot.toml file already exists.")
-    } else { 
-        fs::copy("src/resources/default_config.toml", "nanobot/nanobot.toml").unwrap(); 
+    } else {
+        fs::copy("src/resources/default_config.toml", "nanobot/nanobot.toml").unwrap();
         Ok("Hello world")
     }
 }
@@ -18,20 +16,17 @@ fn main() {
         .propagate_version(true)
         .subcommand_required(true)
         .arg_required_else_help(true)
-        .subcommand(
-            Command::new("init")
-                .about("Initialises things")
-        )
+        .subcommand(Command::new("init").about("Initialises things"))
         .get_matches();
 
     let exit_result = match matches.subcommand() {
-        Some(("init", _sub_matches)) => init() ,
+        Some(("init", _sub_matches)) => init(),
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     };
 
     //print exit message
     match exit_result {
         Err(x) => println!("{}", x),
-        Ok(x) => println!("{}", x), 
+        Ok(x) => println!("{}", x),
     }
 }
