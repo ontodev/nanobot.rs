7777f5aec87adc97a5971c9f03d2b08169d7f595
chris <chris.kind.man@gmail.com>
2022-11-27 00:06:37 +0000

    Add basic `nanobot config` subcommand (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,103 @@
 use clap::{command, Command};
 use std::fs;
 use std::path::Path;
+use toml::map::Map;
+use toml::Value;
 
-fn init() -> Result<&'static str, &'static str> {
+fn init() -> Result<String, String> {
     if Path::new("nanobot.toml").exists() {
-        Err("nanobot.toml file already exists.")
+        Err(String::from("nanobot.toml file already exists."))
     } else {
         fs::copy("src/resources/default_config.toml", "nanobot.toml").unwrap();
-        Ok("Hello world")
+        Ok(String::from("Hello world"))
     }
+}
+
+//Merge two toml::Values (giving priority to the second argument)
+fn merge(v1: &Value, v2: &Value) -> Value {
+    match v1 {
+        Value::Table(x) => match v2 {
+            Value::Table(y) => {
+                let mut merge_table = Map::new();
+                for (k, v) in x {
+                    if y.contains_key(k) {
+                        let yv = y.get(k).unwrap();
+                        let m = merge(v, yv);
+                        merge_table.insert(k.clone(), m);
+                    } else {
+                        merge_table.insert(k.clone(), v.clone());
+                    }
+                }
+                for (k, v) in y {
+                    if !merge_table.contains_key(k) {
+                        merge_table.insert(k.clone(), v.clone());
+                    }
+                }
+                Value::Table(merge_table)
+            }
+            _ => panic!("Cannot merge inconsistent types."),
+        },
+        Value::Array(x) => match v2 {
+            Value::Array(y) => {
+                let mut merged = [&x[..], &y[..]].concat();
+                merged.dedup();
+                Value::Array(merged)
+            }
+            _ => panic!("Cannot merge inconsistent types."),
+        },
+        Value::String(_x) => match v2 {
+            Value::String(y) => Value::String(y.clone()),
+            _ => panic!("Cannot merge inconsistent types."),
+        },
+        Value::Integer(_x) => match v2 {
+            Value::Integer(y) => Value::Integer(y.clone()),
+            _ => panic!("Cannot merge inconsistent types."),
+        },
+        Value::Float(_x) => match v2 {
+            Value::Float(y) => Value::Float(y.clone()),
+            _ => panic!("Cannot merge inconsistent types."),
+        },
+        Value::Boolean(_x) => match v2 {
+            Value::Boolean(y) => Value::Boolean(y.clone()),
+            _ => panic!("Cannot merge inconsistent types."),
+        },
+        Value::Datetime(_x) => match v2 {
+            Value::Datetime(y) => Value::Datetime(y.clone()),
+            _ => panic!("Cannot merge inconsistent types."),
+        },
+    }
+}
+
+fn config(file_path: &str) -> Result<String, String> {
+    let default_config = fs::read_to_string("src/resources/default_config.toml")
+        .expect("Should have been able to read the file");
+
+    let input_config =
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let input_value = input_config.parse::<Value>().unwrap();
+    let default_value = default_config.parse::<Value>().unwrap();
+
+    let value_table = input_value.as_table().unwrap();
+    let default_table = default_value.as_table().unwrap();
+
+    let mut merge_table = Map::new();
+    merge_table.clone_from(&value_table);
+
+    for (k, v) in default_table.iter() {
+        if !value_table.contains_key(k) {
+            merge_table.insert(k.clone(), v.clone());
+        } else {
+            let v2 = value_table.get(k).unwrap();
+            let merged = merge(v, v2);
+            merge_table.insert(k.clone(), merged);
+        }
+    }
+
+    let merge_value = Value::Table(merge_table);
+    let toml = toml::to_string(&merge_value).unwrap();
+
+    Ok(toml)
 }
 
 fn main() {
@@ -17,10 +106,12 @@ fn main() {
         .subcommand_required(true)
         .arg_required_else_help(true)
         .subcommand(Command::new("init").about("Initialises things"))
+        .subcommand(Command::new("config").about("Configures things"))
         .get_matches();
 
     let exit_result = match matches.subcommand() {
         Some(("init", _sub_matches)) => init(),
+        Some(("config", _sub_matches)) => config("nanobot.toml"),
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     };
 
