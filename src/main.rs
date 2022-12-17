f093e760c6d447f609a5e6c82d39a9e705bdfd27
James A. Overton <james@overton.ca>
2022-12-17 16:20:30 +0000

    Change default logging level to WARN

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ async fn main() {
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
         // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
         // will be written to stdout.
-        .with_max_level(tracing::Level::INFO)
+        // .with_max_level(tracing::Level::INFO)
+        .with_max_level(tracing::Level::WARN)
         // completes the builder.
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
