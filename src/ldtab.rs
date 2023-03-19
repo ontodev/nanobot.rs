0013c0340ad1b66d1e38209496e146857153d0b8
chris <chris.kind.man@gmail.com>
2023-03-19 12:47:29 +0000

    Add ordering of predicates (#35)

--- src/ldtab.rs
+++ src/ldtab.rs
@@ -233,6 +233,71 @@ pub fn ldtab_value_to_html(
     Value::Array(list_element)
 }
 
+//Given a predicate map, a label map, a starting and ending list of CURIEs,
+//return the tuple: (order_vector, curie_2_value_map) where
+//the order_vector contains an order of CURIEs by labels (see
+//https://github.com/ontodev/gizmos#predicates for details)
+//the curie_2_value map is a HashMap from property CURIEs to values
+//(this is the same information given by the input predicate map, but encoded in a HashMap rather
+//than a JSON object)
+pub fn sort_predicate_map_by_label(
+    predicate_map: &Value,
+    label_map: &HashMap<String, String>,
+    starting_order: &Vec<String>,
+    ending_order: &Vec<String>,
+) -> (Vec<String>, HashMap<String, Value>) {
+    //let mut keys = Vec::new();
+    let mut keys = HashSet::new(); //this operates on CURIEs
+    let mut label_2_iri = HashMap::new();
+    let mut iri_2_label = HashMap::new();
+    let mut key_2_value = HashMap::new();
+
+    for (key, value) in predicate_map.as_object().unwrap() {
+        let key_label = match label_map.get(key) {
+            Some(x) => x,
+            None => key,
+        };
+
+        keys.insert(key);
+        iri_2_label.insert(key, key_label);
+        label_2_iri.insert(key_label, key.clone());
+        key_2_value.insert(key.clone(), value.clone());
+    }
+
+    let mut middle_order = Vec::new(); //this holds labels .. because we order by label
+
+    for key in starting_order {
+        if keys.contains(key) {
+            keys.remove(key);
+        }
+    }
+    for key in ending_order {
+        if keys.contains(key) {
+            keys.remove(key);
+        }
+    }
+    for key in keys {
+        let label = iri_2_label.get(key).unwrap();
+        middle_order.push(label);
+    }
+
+    middle_order.sort();
+
+    let mut order = Vec::new();
+    for key in starting_order {
+        order.push(key.clone());
+    }
+    for key in middle_order {
+        let iri = label_2_iri.get(key).unwrap();
+        order.push(iri.clone());
+    }
+    for key in ending_order {
+        order.push(key.clone());
+    }
+
+    (order, key_2_value)
+}
+
 pub async fn get_predicate_map_hiccup(subject: &str, table: &str, pool: &SqlitePool) -> Value {
     let predicate_map = get_property_map(subject, table, pool).await.unwrap();
 
@@ -247,21 +312,35 @@ pub async fn get_predicate_map_hiccup(subject: &str, table: &str, pool: &SqliteP
     outer_list.push(json!("ul"));
     outer_list.push(json!({"id":"annotations", "style" : "margin-left: -1rem;"}));
 
-    for (key, value) in predicate_map.as_object().unwrap() {
+    //Give precedence to labels, definitions, and synonyms (TODO)
+    let starting_order = vec![String::from("rdfs:label"), String::from("obo:IAO_0000115")];
+    //Put comments last
+    let ending_order = vec![String::from("rdfs:comment")];
+
+    let (order, key_2_value) =
+        sort_predicate_map_by_label(&predicate_map, &label_map, &starting_order, &ending_order);
+
+    for key in order {
+        if !key_2_value.contains_key(&key) {
+            continue;
+        }
+
+        let value = key_2_value.get(&key).unwrap();
+
         let mut outer_list_element = Vec::new();
 
-        let key_label = match label_map.get(key) {
-            Some(x) => x,
-            None => key,
+        let key_label = match label_map.get(&key) {
+            Some(x) => x.clone(),
+            None => key.clone(),
         };
 
         outer_list_element.push(json!("li"));
-        outer_list_element.push(json!(["a", { "resource": key }, key_label]));
+        outer_list_element.push(json!(["a", { "resource": key.clone() }, key_label]));
 
         let mut inner_list = Vec::new();
         inner_list.push(json!("ul"));
         for v in value.as_array().unwrap() {
-            let v_encoding = ldtab_value_to_html(key, v, &label_map);
+            let v_encoding = ldtab_value_to_html(&key, v, &label_map);
             inner_list.push(json!(v_encoding));
         }
         outer_list_element.push(json!(inner_list));
