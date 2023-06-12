29e0c38535ac5c7782e24a538f629530968c15e8
James A. Overton <james@overton.ca>
2023-06-12 21:46:46 +0000

    Update VALVE, log level

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,9 @@ async fn main() {
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
         // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
         // will be written to stdout.
-        .with_max_level(tracing::Level::INFO)
-        //.with_max_level(tracing::Level::WARN)
-        //.with_max_level(tracing::Level::DEBUG)
+        .with_max_level(tracing::Level::WARN)
+        // .with_max_level(tracing::Level::INFO)
+        // .with_max_level(tracing::Level::DEBUG)
         // completes the builder.
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
